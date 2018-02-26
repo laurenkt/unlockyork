bda805c0a7b52340d125f47c83375447b5219988
Lauren <lauren@tomasello.me>
2018-02-26 19:26:10 +0000

    Add video player test

--- kiosk/Kiosk.java
+++ kiosk/Kiosk.java
@@ -4,24 +4,28 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
 public class Kiosk extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(event -> System.out.println("Hello World!"));
-
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
 
-        Scene scene = new Scene(root, 300, 250);
+        MediaPlayer player = new MediaPlayer( new Media(getClass().getResource("unlockyork.mp4").toExternalForm()));
+        MediaView mediaView = new MediaView(player);
+
+        root.getChildren().add( mediaView);
+
+        Scene scene = new Scene(root,700, 700);
 
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+        player.play();
     }
     public static void main(String[] args) {
         launch(args);
