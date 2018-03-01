4efb8b4ba610b2937e6608f08098a0b68ca11bea
Lauren <lauren@tomasello.me>
2018-03-01 18:52:33 +0000

    Add JDom

--- src/main/java/XMLParser.java
+++ src/main/java/XMLParser.java
@@ -1,11 +1,10 @@
-/*
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 public class XMLParser {
 
@@ -16,4 +15,3 @@ public class XMLParser {
 
     }
 }
-*/
