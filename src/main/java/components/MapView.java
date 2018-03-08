bcf5c8928e15af24d8cff1b9748116d9839d5a75
Lauren <lauren@tomasello.me>
2018-03-08 14:36:31 +0000

    Refactor MapView to be a ScrollPane

--- src/main/java/components/MapView.java
+++ src/main/java/components/MapView.java
@@ -13,30 +13,20 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class MapView extends StackPane {
-    public MapView(Stage stage, Image tiles) {
+public class MapView extends ScrollPane {
+    public MapView(Image tiles) {
         super();
 
+        setPannable(true);
+        setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+        StackPane stackPane = new StackPane();
+        setContent(stackPane);
+        stackPane.getChildren().setAll(new ImageView(tiles));
 
         // set the image as the child
         getChildren().setAll(new ImageView(tiles));
-
-        ScrollPane scroll = createScrollPane();
-        Scene scene = new Scene(scroll);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    private ScrollPane createScrollPane() {
-        ScrollPane scrollPane = new ScrollPane();
-        // Make sure the content can be panned
-        scrollPane.setPannable(true);
-        // Hide the scroll bars
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        // link it to this stackpane
-        scrollPane.setContent(this);
-        return scrollPane;
     }
 
 }
