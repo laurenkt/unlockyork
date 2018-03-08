20c06f58858becd050787afd3b978441b416eedf
Lauren <lauren@tomasello.me>
2018-03-08 15:05:42 +0000

    Add zooming to the scrollpane

--- src/main/java/components/MapView.java
+++ src/main/java/components/MapView.java
@@ -5,28 +5,88 @@ import com.sun.javafx.geom.transform.BaseTransform;
 import com.sun.javafx.jmx.MXNodeAlgorithm;
 import com.sun.javafx.jmx.MXNodeAlgorithmContext;
 import com.sun.javafx.sg.prism.NGNode;
+import javafx.geometry.Bounds;
+import javafx.geometry.Point2D;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 
 public class MapView extends ScrollPane {
+    private double scaleValue = 0.7;
+    private double zoomIntensity = 0.02;
+    private Node target;
+    private Node zoomNode;
+
     public MapView(Image tiles) {
         super();
+        this.target = new ImageView(tiles);
+        this.zoomNode = new Group(target);
+        setContent(outerNode(zoomNode));
 
         setPannable(true);
         setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        setFitToHeight(true); //center
+        setFitToWidth(true); //center
 
-        StackPane stackPane = new StackPane();
-        setContent(stackPane);
-        stackPane.getChildren().setAll(new ImageView(tiles));
+        updateScale();
+    }
 
-        // set the image as the child
-        getChildren().setAll(new ImageView(tiles));
+    private Node outerNode(Node node) {
+        Node outerNode = centeredNode(node);
+        outerNode.setOnScroll(e -> {
+            e.consume();
+            onScroll(e.getDeltaY(), new Point2D(e.getX(), e.getY()));
+        });
+        return outerNode;
+    }
+
+    private Node centeredNode(Node node) {
+        VBox vBox = new VBox(node);
+        vBox.setAlignment(Pos.CENTER);
+        return vBox;
+    }
+
+    private void updateScale() {
+        target.setScaleX(scaleValue);
+        target.setScaleY(scaleValue);
+    }
+
+    private void onScroll(double wheelDelta, Point2D mousePoint) {
+        double zoomFactor = Math.exp(wheelDelta * zoomIntensity);
+
+        System.out.println(wheelDelta);
+
+        Bounds innerBounds = zoomNode.getLayoutBounds();
+        Bounds viewportBounds = getViewportBounds();
+
+        // calculate pixel offsets from [0, 1] range
+        double valX = this.getHvalue() * (innerBounds.getWidth() - viewportBounds.getWidth());
+        double valY = this.getVvalue() * (innerBounds.getHeight() - viewportBounds.getHeight());
+
+        scaleValue = scaleValue * zoomFactor;
+        updateScale();
+        this.layout(); // refresh ScrollPane scroll positions & target bounds
+
+        // convert target coordinates to zoomTarget coordinates
+        Point2D posInZoomTarget = target.parentToLocal(zoomNode.parentToLocal(mousePoint));
+
+        // calculate adjustment of scroll position (pixels)
+        Point2D adjustment = target.getLocalToParentTransform().deltaTransform(posInZoomTarget.multiply(zoomFactor - 1));
+
+        // convert back to [0, 1] range
+        // (too large/small values are automatically corrected by ScrollPane)
+        Bounds updatedInnerBounds = zoomNode.getBoundsInLocal();
+        this.setHvalue((valX + adjustment.getX()) / (updatedInnerBounds.getWidth() - viewportBounds.getWidth()));
+        this.setVvalue((valY + adjustment.getY()) / (updatedInnerBounds.getHeight() - viewportBounds.getHeight()));
     }
 
 }
