e2353afbedb172b652577014834f62aca8bca404
Liam <mcmahonlt96@aol.com>
2018-03-08 11:46:00 +0000

    added mouse functions to deal with varies mouse events in the java application

--- src/main/java/components/MapView.java
+++ src/main/java/components/MapView.java
@@ -5,6 +5,8 @@ import com.sun.javafx.geom.transform.BaseTransform;
 import com.sun.javafx.jmx.MXNodeAlgorithm;
 import com.sun.javafx.jmx.MXNodeAlgorithmContext;
 import com.sun.javafx.sg.prism.NGNode;
+import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
@@ -13,16 +15,67 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.awt.event.MouseEvent;
+
 public class MapView extends StackPane {
     public MapView(Stage stage, Image tiles) {
         super();
-
 
         // set the image as the child
         getChildren().setAll(new ImageView(tiles));
 
         ScrollPane scroll = createScrollPane();
         Scene scene = new Scene(scroll);
+
+        scene.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
+            @Override
+            public void handle(javafx.scene.input.MouseEvent event) {
+                System.out.println("Mouse Clicked");
+            }
+        });
+
+        scene.setOnMouseDragged(new EventHandler<javafx.scene.input.MouseEvent>() {
+            @Override
+            public void handle(javafx.scene.input.MouseEvent event) {
+                System.out.println("Mouse Dragged");
+            }
+        });
+
+        scene.setOnMouseEntered(new EventHandler<javafx.scene.input.MouseEvent>() {
+            @Override
+            public void handle(javafx.scene.input.MouseEvent event) {
+                System.out.println("Mouse Entered");
+            }
+        });
+
+        scene.setOnMouseExited(new EventHandler<javafx.scene.input.MouseEvent>() {
+            @Override
+            public void handle(javafx.scene.input.MouseEvent event) {
+                System.out.println("Mouse Exited");
+            }
+        });
+
+        scene.setOnMouseMoved(new EventHandler<javafx.scene.input.MouseEvent>() {
+            @Override
+            public void handle(javafx.scene.input.MouseEvent event) {
+                System.out.println("Mouse Moved");
+            }
+        });
+
+        scene.setOnMousePressed(new EventHandler<javafx.scene.input.MouseEvent>() {
+            @Override
+            public void handle(javafx.scene.input.MouseEvent event) {
+                System.out.println("Mouse Presssed");
+            }
+        });
+
+        scene.setOnMouseReleased(new EventHandler<javafx.scene.input.MouseEvent>() {
+            @Override
+            public void handle(javafx.scene.input.MouseEvent event) {
+                System.out.println("Mouse Released");
+            }
+        });
+
         stage.setScene(scene);
         stage.show();
     }
@@ -38,5 +91,5 @@ public class MapView extends StackPane {
         scrollPane.setContent(this);
         return scrollPane;
     }
-
 }
+
