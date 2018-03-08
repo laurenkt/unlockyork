69f7c223433f212e8d6a65c1bbe63e9198a9ea4e
Ollie <om642@york.ac.uk>
2018-03-08 16:27:03 +0000

    GPS class

---
+++ src/main/java/models/GPS.java
@@ -0,0 +1,16 @@
+package models;
+
+public class GPS {
+
+    double longitude;
+    double latitude;
+
+    double elevation;
+
+    GPS(double longitude, double latitude, double elevation)
+    {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.elevation = elevation;
+    }
+}
