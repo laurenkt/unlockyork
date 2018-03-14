94e90f22ced06b851503cb6caeab40bb18215e03
jameswinters <jw1857@york.ac.uk>
2018-03-14 12:22:08 +0000

    Added test class from Android Studio, and resolved the dependency issues for @SmallTest and espresso libraries

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
 
+
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
