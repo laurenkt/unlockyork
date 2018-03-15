8d76d8e6591666a9dc0cc91a4df5ed4e13c75d47
Ollie <om642@york.ac.uk>
2018-03-15 21:09:07 +0000

    Merge branch 'master' of https://github.com/laurenkt/unlockyork
    # Conflicts:
    #	src/main/java/XMLParser.java

--- src/test/java/XMLParserTest.java
+++ src/test/java/XMLParserTest.java
@@ -1,21 +1,38 @@
+import models.Presentation;
+import models.Shape;
+import models.Slide;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.swing.text.html.parser.Parser;
+
 import static org.junit.Assert.assertTrue;
+
 
 public class XMLParserTest {
     XMLParser parser;
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         parser = new XMLParser();
     }
 
     @Test
-    public void onePlusOneEqualsTwo() {
-        assertTrue(1+1 == 2);
+    public void testPresentation() {
+        XMLParser parser = new XMLParser();
+        Presentation presentation = parser.parser("src/build/resources/main/example.pws", "src/build/resources/main/schema.xsd");
+
+        assertTrue(presentation.getPresDefaultFont().isItalic() == false);
+        assertTrue(presentation.getPresDefaultFont().isUnderline() == false);
+        assertTrue(presentation.getPresDefaultFont().getTextSize() == 12);
+        assertTrue(presentation.getSlides().size() == 5);
     }
 
+    @Test
+    public void onePlusOneEqualsTwo() {
+        assertTrue(1 + 1 == 2);
+    }
 }
+
 
 
