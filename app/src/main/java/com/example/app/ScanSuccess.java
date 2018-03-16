1d3e756da17cd18ebc9fbc4cee7d5bae68061d08
Lauren <lauren@tomasello.me>
2018-03-16 11:00:14 +0000

    Amend gradle config to fix build problems

--- app/src/main/java/com/example/app/ScanSuccess.java
+++ app/src/main/java/com/example/app/ScanSuccess.java
@@ -1,8 +1,8 @@
 package com.example.app;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
