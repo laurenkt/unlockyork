eb8dbd354d49ec90d890a180f87f0f1aba349ec8
Liam <mcmahonlt96@aol.com>
2018-04-24 12:07:55 +0100

    Linked presentation view to Kiosk

--- src/main/java/Kiosk.java
+++ src/main/java/Kiosk.java
@@ -92,17 +92,20 @@ public class Kiosk extends Application {
         Button audioTour = new Button("Audio Tour");
         Button Video = new Button("Video");
         Button SubMap = new Button("Sub-map");
+        Button Presentataion = new Button("Presentation");
 
         HBox.setHgrow(back, Priority.ALWAYS);
         HBox.setHgrow(audioTour, Priority.ALWAYS);
         HBox.setHgrow(Video, Priority.ALWAYS);
         HBox.setHgrow(SubMap, Priority.ALWAYS);
+        HBox.setHgrow(Presentataion, Priority.ALWAYS);
         back.setMaxWidth(Double.MAX_VALUE);
         audioTour.setMaxWidth(Double.MAX_VALUE);
         Video.setMaxWidth(Double.MAX_VALUE);
         SubMap.setMaxWidth(Double.MAX_VALUE);
+        Presentataion.setMaxWidth(Double.MAX_VALUE);
 
-        UserButtons.getChildren().addAll(back,audioTour,Video,SubMap);
+        UserButtons.getChildren().addAll(back,audioTour,Video,SubMap,Presentataion);
         UserButtons.setAlignment(Pos.CENTER);
 
         Image YorkMinister = new Image(getClass().getClassLoader().getResource("YorkMinister.jpg").toExternalForm());
@@ -142,6 +145,17 @@ public class Kiosk extends Application {
             }
         });
         SubMap.setOnAction(e -> poiInfo.setCenter(YorkMinisterImage));
+        Presentataion.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                PresentationView view = new PresentationView();
+                try {
+                    view.start(primaryStage);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
