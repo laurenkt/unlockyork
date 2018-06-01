2ea4d3fd589d58510bb7c98b82a5bf49193459ff
Lauren <lauren@tomasello.me>
2018-06-01 10:40:17 +0100

    Replace map tiles

--- src/main/java/components/MapView.java
+++ src/main/java/components/MapView.java
@@ -57,7 +57,6 @@ public class MapView extends ScrollPane {
         tiles.add(new Image(getClass().getResource("/tiles/19.png").toExternalForm()));
         tiles.add(new Image(getClass().getResource("/tiles/20.png").toExternalForm()));
         tiles.add(new Image(getClass().getResource("/tiles/21.png").toExternalForm()));
-        tiles.add(new Image(getClass().getResource("/tiles/22.png").toExternalForm()));
 
         mapView = new ImageView();
         mapView.setImage(tiles.get(level));
@@ -100,6 +99,7 @@ public class MapView extends ScrollPane {
             poi.setScaleX(0.3 / val.doubleValue());
             poi.setScaleY(0.3 / val.doubleValue());
             this.setLevel((int)(4*val.doubleValue() - 1));
+            System.out.println(val.doubleValue());
         });
 
         setScaleValue(scaleValue, 0, 0);
@@ -239,7 +239,7 @@ public class MapView extends ScrollPane {
 
         // Bounded scale value
         setScaleValue(
-                Math.min(1.15, Math.max(0.45, scaleValue * zoomFactor)),
+                Math.min(1.5, Math.max(0.25, scaleValue * zoomFactor)),
                 (valX + adjustment.getX()) / (updatedInnerBounds.getWidth() - viewportBounds.getWidth()),
                 (valY + adjustment.getY()) / (updatedInnerBounds.getHeight() - viewportBounds.getHeight())
         );
