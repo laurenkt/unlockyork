b5c92b10fa0207f4052d50dcb262a1e387d46725
Lauren <lauren@tomasello.me>
2018-06-06 19:26:31 +0100

    Display finance docs on tour

--- tour/src/document.js
+++ tour/src/document.js
@@ -10,16 +10,45 @@ export default {
   "Design Documentation": "Test",
   "Minutes": "Test",
   "Time-sheets": "Test",
-  "Finance Reports and Summary": "Test",
+  "Finance": [
+      {
+          name: 'Briefing',
+          children: [
+              {name:'Covering Note', type:'pdf', path:'documents/finance/briefing/covering-note.pdf'},
+              {name:'Briefing', type:'pdf', path:'documents/finance/briefing/briefing.pdf'},
+          ]
+      },
+      {
+          name: 'Report 1',
+          children: [
+              {name:'Covering Note', type:'pdf', path:'documents/finance/report-1/cover-sheet.pdf'},
+              {name:'Report 1', type:'pdf', path:'documents/finance/report-1/report-1.pdf'},
+          ]
+      },
+      {
+          name: 'Report 2',
+          children: [
+              {name:'Covering Note', type:'pdf', path:'documents/finance/report-2/cover-sheet.pdf'},
+              {name:'Report 2', type:'pdf', path:'documents/finance/report-2/report-2.pdf'},
+          ]
+      },
+      {
+          name: 'Report 3',
+          children: [
+              {name:'Covering Note', type:'pdf', path:'documents/finance/report-3/cover-sheet.pdf'},
+              {name:'Report 3', type:'pdf', path:'documents/finance/report-3/report-3.pdf'},
+          ]
+      }
+  ],
   "Project Management": [
       {name: 'GANTT', content: require('../documents/project-management/gantt/gantt.md')},
       {name: 'PERT', type: 'pdf', path: 'documents/project-management/pert/pert.pdf'},
       {name: 'WBS', children: [
-              {name: 'WBS/1', type:'pdf', path:'documents/project-management/wbs/1.pdf'},
-              {name: 'WBS/1.1', type:'pdf', path:'documents/project-management/wbs/1.1.pdf'},
-              {name: 'WBS/2', type:'pdf', path:'documents/project-management/wbs/2.pdf'},
-              {name: 'WBS/2.1', type:'pdf', path:'documents/project-management/wbs/2.1.pdf'},
-              {name: 'WBS/3', type:'pdf', path:'documents/project-management/wbs/3.pdf'},
+          {name: 'WBS/1', type:'pdf', path:'documents/project-management/wbs/1.pdf'},
+          {name: 'WBS/1.1', type:'pdf', path:'documents/project-management/wbs/1.1.pdf'},
+          {name: 'WBS/2', type:'pdf', path:'documents/project-management/wbs/2.pdf'},
+          {name: 'WBS/2.1', type:'pdf', path:'documents/project-management/wbs/2.1.pdf'},
+          {name: 'WBS/3', type:'pdf', path:'documents/project-management/wbs/3.pdf'},
       ]},
   ],
   "Contracts": [
